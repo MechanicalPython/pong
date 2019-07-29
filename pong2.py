3ca867efb468a844d0924192744aea6053379ce1
Matt Barson <matt.barson@gmail.com>
2019-07-29 16:39:35 +0100

    initial commit from pi

---
+++ pong2.py
@@ -0,0 +1,266 @@
+# Pong
+# Language - Python
+# Modules - pygame, sys, random, math
+#
+# Controls - Arrow Keys for Right Paddle and WASD Keys for Left Paddle
+
+import pygame
+import pygame.freetype
+import sys
+import random
+from math import *
+import os
+
+import read_paddle
+
+pygame.init()
+
+width = 900
+height = 1000
+
+display = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Pong!")
+clock = pygame.time.Clock()
+
+background = (0, 0, 0)
+white = (236, 240, 241)
+
+top = white
+bottom = white
+left = white
+right = white
+
+margin = 4
+
+scoreLeft = 0
+scoreRight = 0
+maxScore = 11
+
+font = pygame.freetype.Font('SF Atarian System Extended Bold.ttf', 60)
+
+
+# Draw the Boundary of Board
+def boundary():
+    global top, bottom, left, right
+    pygame.draw.rect(display, left, (0, 0, margin, height))
+    pygame.draw.rect(display, top, (0, 0, width, margin))
+    pygame.draw.rect(display, right, (width - margin, 0, margin, height))
+    pygame.draw.rect(display, bottom, (0, height - margin, width, margin))
+
+    dash_height = 15
+    dash_width = 1
+
+    number_of_dashes = int(height/(dash_height*2))
+    x = width / 2 - margin / 2
+    y = 0
+    for dash in range(number_of_dashes):
+        pygame.draw.rect(display, white, (x, y, dash_width, dash_height))  # screen, colour, (x, y, width, height)
+        y += dash_height*2
+
+
+# Paddle Class
+class Paddle:
+    def __init__(self, position):
+        self.w = 10
+        self.h = self.w * 8
+        self.paddleSpeed = 6
+
+        if position == -1:
+            self.x = 1.5 * margin
+        else:
+            self.x = width - 1.5 * margin - self.w
+
+        self.y = height / 2 - self.h / 2
+
+    # Show the Paddle
+    def show(self):
+        pygame.draw.rect(display, white, (self.x, self.y, self.w, self.h))
+
+    # Move the Paddle
+    def move(self, ydir):
+        self.y += self.paddleSpeed * ydir
+        if self.y < 0:
+            self.y -= self.paddleSpeed * ydir
+        elif self.y + self.h > height:
+            self.y -= self.paddleSpeed * ydir
+
+
+leftPaddle = Paddle(-1)
+rightPaddle = Paddle(1)
+
+
+# Ball Class
+class Ball:
+    def __init__(self, color):
+        self.r = 20
+        self.x = width / 2 - self.r / 2
+        self.y = height / 2 - self.r / 2
+        self.color = color
+        self.angle = random.randint(-75, 75)
+        if random.randint(0, 1):
+            self.angle += 180
+
+        self.speed = 8
+
+    # Show the Ball
+    def show(self):
+        pygame.draw.ellipse(display, self.color, (self.x, self.y, self.r, self.r))
+
+    # Move the Ball
+    def move(self):
+        global scoreLeft, scoreRight
+        self.x += self.speed * cos(radians(self.angle))
+        self.y += self.speed * sin(radians(self.angle))
+        if self.x + self.r > width - margin:
+            scoreLeft += 1
+            self.angle = 180 - self.angle
+        if self.x < margin:
+            scoreRight += 1
+            self.angle = 180 - self.angle
+        if self.y < margin:
+            self.angle = - self.angle
+        if self.y + self.r >= height - margin:
+            self.angle = - self.angle
+
+    # Check and Reflect the Ball when it hits the padddle
+    def checkForPaddle(self):
+        if self.x < width / 2:
+            if leftPaddle.x < self.x < leftPaddle.x + leftPaddle.w:
+                if leftPaddle.y < self.y < leftPaddle.y + 10 or leftPaddle.y < self.y + self.r < leftPaddle.y + 10:
+                    self.angle = -45
+                if leftPaddle.y + 10 < self.y < leftPaddle.y + 20 or leftPaddle.y + 10 < self.y + self.r < leftPaddle.y + 20:
+                    self.angle = -30
+                if leftPaddle.y + 20 < self.y < leftPaddle.y + 30 or leftPaddle.y + 20 < self.y + self.r < leftPaddle.y + 30:
+                    self.angle = -15
+                if leftPaddle.y + 30 < self.y < leftPaddle.y + 40 or leftPaddle.y + 30 < self.y + self.r < leftPaddle.y + 40:
+                    self.angle = -10
+                if leftPaddle.y + 40 < self.y < leftPaddle.y + 50 or leftPaddle.y + 40 < self.y + self.r < leftPaddle.y + 50:
+                    self.angle = 10
+                if leftPaddle.y + 50 < self.y < leftPaddle.y + 60 or leftPaddle.y + 50 < self.y + self.r < leftPaddle.y + 60:
+                    self.angle = 15
+                if leftPaddle.y + 60 < self.y < leftPaddle.y + 70 or leftPaddle.y + 60 < self.y + self.r < leftPaddle.y + 70:
+                    self.angle = 30
+                if leftPaddle.y + 70 < self.y < leftPaddle.y + 80 or leftPaddle.y + 70 < self.y + self.r < leftPaddle.y + 80:
+                    self.angle = 45
+        else:
+            if rightPaddle.x + rightPaddle.w > self.x + self.r > rightPaddle.x:
+                if rightPaddle.y < self.y < leftPaddle.y + 10 or leftPaddle.y < self.y + self.r < leftPaddle.y + 10:
+                    self.angle = -135
+                if rightPaddle.y + 10 < self.y < rightPaddle.y + 20 or rightPaddle.y + 10 < self.y + self.r < rightPaddle.y + 20:
+                    self.angle = -150
+                if rightPaddle.y + 20 < self.y < rightPaddle.y + 30 or rightPaddle.y + 20 < self.y + self.r < rightPaddle.y + 30:
+                    self.angle = -165
+                if rightPaddle.y + 30 < self.y < rightPaddle.y + 40 or rightPaddle.y + 30 < self.y + self.r < rightPaddle.y + 40:
+                    self.angle = 170
+                if rightPaddle.y + 40 < self.y < rightPaddle.y + 50 or rightPaddle.y + 40 < self.y + self.r < rightPaddle.y + 50:
+                    self.angle = 190
+                if rightPaddle.y + 50 < self.y < rightPaddle.y + 60 or rightPaddle.y + 50 < self.y + self.r < rightPaddle.y + 60:
+                    self.angle = 165
+                if rightPaddle.y + 60 < self.y < rightPaddle.y + 70 or rightPaddle.y + 60 < self.y + self.r < rightPaddle.y + 70:
+                    self.angle = 150
+                if rightPaddle.y + 70 < self.y < rightPaddle.y + 80 or rightPaddle.y + 70 < self.y + self.r < rightPaddle.y + 80:
+                    self.angle = 135
+
+
+# Show the Score
+def showScore():
+    font.render_to(display, ((width/4, 20)), str(scoreLeft), white)
+    font.render_to(display, (((width / 4)*3, 20)), str(scoreRight), white)
+
+# Game Over
+def gameOver():
+    if scoreLeft == maxScore or scoreRight == maxScore:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    close()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        close()
+                    if event.key == pygame.K_r:
+                        reset()
+            font.render_to(display, (width / 2 - 185, height / 2), "GAME OVER", white)
+            pygame.display.update()
+
+
+def reset():
+    global scoreLeft, scoreRight
+    scoreLeft = 0
+    scoreRight = 0
+    board()
+
+
+def close():
+    pygame.quit()
+    sys.exit()
+
+
+def auto_paddle(paddle, side):
+    move = False
+    ball_angle = abs(ball.angle)
+    if side == 'left' and ball_angle > 90:
+        move = True
+    elif side == 'right' and ball_angle < 90:
+        move = True
+    pc = paddle.y + (paddle.h/2)
+    bc = ball.y
+
+    if pc > bc and move:
+        return -1
+    elif pc < bc and move:
+        return 1
+    else:
+        return 0
+
+
+def board():
+    pygame.event.set_allowed([pygame.KEYDOWN])
+    pygame.mouse.set_visible(False)
+    display.set_alpha(None)
+    loop = True
+    leftChange = 0
+    rightChange = 0
+    global ball
+    ball = Ball(white)
+    while loop:
+        #left_paddle_event = read_paddle.move(18, 23)
+        #right_paddle_event = read_paddle.move(7, 8)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                        close()
+
+        #if left_paddle_event == 'UP':
+        #    leftChange = 1
+        #elif left_paddle_event == 'DOWN':
+        #    leftChange = -1
+        #elif left_paddle_event is None:
+        leftChange = auto_paddle(leftPaddle, 'left')
+        #if right_paddle_event == 'UP':
+        #    rightChange = 1
+        #elif right_paddle_event == 'DOWN':
+        #    rightChange = -1
+        #elif right_paddle_event is None:
+        rightChange = auto_paddle(rightPaddle, 'right')
+
+        leftPaddle.move(leftChange)
+        rightPaddle.move(rightChange)
+        ball.move()
+        ball.checkForPaddle()
+
+        display.fill(background)
+        showScore()
+
+        ball.show()
+        leftPaddle.show()
+        rightPaddle.show()
+
+        boundary()
+
+        gameOver()
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+board()
---
+++ read_paddle.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+import math
+
+
+def switch(switch):
+    if GPIO.input(switch) == GPIO.HIGH:
+        print('ON')
+        time.sleep(0.1)
+
+
+def discharge(pin1, pin2):
+    GPIO.setup(pin1, GPIO.IN)
+    GPIO.setup(pin2, GPIO.OUT)
+    GPIO.output(pin2, False)
+    time.sleep(0.01)
+
+
+def charge_time(pin1, pin2):
+    GPIO.setup(pin2, GPIO.IN)
+    GPIO.setup(pin1, GPIO.OUT)
+    GPIO.output(pin1, True)
+    t1 = time.time()
+    while not GPIO.input(pin2):
+        if time.time() - t1 > 0.1:
+            return None
+        pass
+    t2 = time.time()
+    return t2-t1
+
+
+def move(pin1, pin2): 
+    GPIO.setmode(GPIO.BCM)
+    discharge(pin1, pin2)
+    t = charge_time(pin1, pin2)
+    discharge(pin1, pin2)
+    if t is None:
+        GPIO.cleanup()
+        return None
+    if t > 0.069:  # Bigger therfore full left
+        GPIO.cleanup()
+        return 'UP'
+    if t < 0.069:
+        GPIO.cleanup()
+        return 'DOWN'
+
+
+
+
