7ce173f75da9a18eec3f7982bc493aac63a52010
Matt Barson <matt.barson@gmail.com>
2019-08-04 09:12:50 +0100

    Remove timer

--- read_paddle.py
+++ read_paddle.py
@@ -36,14 +36,12 @@ class PaddleMove:
         if side == 'r':
             self.pin1, self.pin2 = right1, right2
 
-    @timer
     def discharge(self):           # Total 0.000122  Actual is 3-10 times longer.
         GPIO.setup(self.pin1, GPIO.IN)   # 0.00004
         GPIO.setup(self.pin2, GPIO.OUT)  # 0.00004
         GPIO.output(self.pin2, False)    # 0.000032
         time.sleep(0.00001)              # 0.00001
 
-    @timer
     def charge_time(self):         # Total 0.000113
         GPIO.setup(self.pin2, GPIO.IN)   # 0.00004
         GPIO.setup(self.pin1, GPIO.OUT)  # 0.00004
@@ -54,14 +52,12 @@ class PaddleMove:
         t2 = time.time()
         return t2 - t1
 
-    @timer
     def exact_time(self):  # Charge time for one capacitor
         self.discharge()
         t = self.charge_time()
         # self.discharge()
         return t
 
-    @timer
     def avg_charge_time(self, iters=10):
         GPIO.setmode(GPIO.BCM)
         total = []
@@ -79,7 +75,6 @@ class PaddleMove:
             print(self.exact_time())
         GPIO.cleanup()
 
-    @timer
     def position(self):
         """
         Return a number between 0 and 1. 1 is max left (down) and 0 is max right (up). Refelcts y axis on pygame.
